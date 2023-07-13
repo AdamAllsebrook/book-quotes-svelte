b0409572baa1524b2fdd2c2cf939636447fcce58
Adam Allsebrook <adam.allsebrook@gmail.com>
2023-07-13 18:07:32 +0100

    Recreate project using skeleton

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    test: {
-        include: ['src/**/*.{test,spec}.{js,ts}']
-    },
-    resolve: {
-        alias: {
-            '$lib': '/src/lib',
-        }
-    }
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
 });
